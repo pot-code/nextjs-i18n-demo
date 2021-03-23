c4d9320a031749129cc62e2c6e695c070ddc54d8
pot-code <136290354@qq.com>
2021-03-23 21:10:19 +0800

    init commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import { Trans, useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const { t } = useTranslation('common');
   return (
     <div className={styles.container}>
       <Head>
@@ -11,41 +15,39 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <Trans i18nKey="welcome">
+            <a href="https://nextjs.org">Next.js!</a>
+          </Trans>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <Trans i18nKey="get-started">
+            <code className={styles.code}>pages/index.js</code>
+          </Trans>
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>{t('grid.document')} &rarr;</h3>
+            <p>{t('grid.document-desc')}</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h3>{t('grid.learn')} &rarr;</h3>
+            <p>{t('grid.learn-desc')}</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
+            <h3>{t('grid.example')} &rarr;</h3>
+            <p>{t('grid.example-desc')}</p>
           </a>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <h3>{t('grid.deploy')} &rarr;</h3>
+            <p>{t('grid.deploy-desc')}</p>
           </a>
         </div>
       </main>
@@ -56,10 +58,15 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
+
+export const getStaticProps = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(locale, ['common'])),
+  },
+});
